ba9f314810312c4797e5d68a3798e0d33b6b6dd6
S de Wit <dev@sjorsdewit.name>
2017-04-19 11:33:56 +0200

    Add make target 'update'
    'make update' should call firmware_update.py.
    The update script communicates with another process that handles the
    actual update, for example via USB.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Firmware tools
+Tools for updating firmware via USB.
+
+This project is intended to be used as a dependency for a firmware CPM package.
+It adds the Make target 'update' which updates connected USB devices with the
+built firmware.
+
+## requirements
+- Python >= 3.5 (lower versions probably work fine as well)
+- compatible firmware update server program
+
---
+++ firmware_tools/firmware_update.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+
+import time
+import select
+import socket
+import sys
+import os.path
+import argparse
+
+
+def list_to_str(l, sep=','):
+    ret = ""
+    for i in l:
+        ret+= str(i) + sep
+    
+    if len(ret):
+        ret = ret[:-1]
+
+    return ret
+
+def parse_list(l):
+    ret = []
+    for v in l.split(','):
+        val = v.strip()
+        if len(val):
+            ret.append(val)
+
+    return ret
+
+def encode(string):
+    return bytes(string, 'ascii')
+
+def decode(bytestr):
+    return str(bytestr, 'ascii')
+
+def print_devices(devices):
+    for dev in devices:
+        print("\t{}".format(dev))
+    print('')
+
+class Update:
+
+    def __init__(self, firmware_file_m0, firmware_file_m4, host, port):
+        self.devices = []
+        self.updated = []
+        self.firmware_file_m0 = firmware_file_m0
+        self.firmware_file_m4 = firmware_file_m4
+
+        self.server_host = host
+        self.server_port = port
+
+    def parse_incoming(self, socket, timeout_sec=10):
+
+        ready = select.select([socket], [], [], timeout_sec)
+        timeout = True
+        if ready[0]:
+            response = decode(socket.recv(1024*1024))
+            if len(response):
+                timeout = False
+
+                for line in response.split("\n"):
+                    tokens = line.split('=')
+                    if len(tokens) < 2:
+                        continue
+                    key = tokens[0]
+                    value = line[len(key):].strip('=')
+
+                    if key == 'devices':
+                        self.devices = parse_list(value)
+
+                    if key == 'updated':
+                        self.updated = parse_list(value)
+
+        if timeout:
+            print("ERROR: timeout")
+            return False
+        return True
+
+
+    def ui_select_devices(self):
+        """ UI: show devices / select which devices to update """
+
+        to_update = self.devices
+        print("")
+        if not len(self.devices):
+            print("ERROR: No device(s) available for update!")
+            return []
+
+        elif len(self.devices) == 1:
+            print("Updating device {}".format(self.devices[0]))
+
+        else:
+            print("Device(s) available for update:")
+            print_devices(self.devices)
+            
+            # TODO maybe a way to select which ones to update
+            # and / or ENTER to update all?
+        
+        # always update all devices for now
+        return to_update
+   
+
+    def ui_result(self):
+        """ UI: show the result(s) of the update(s)"""
+        failed = []
+        for dev in self.devices:
+            if not dev in self.updated:
+                failed.append(dev)
+        
+        if len(failed):
+            print("ERROR: Failed to update these devices:")
+            print_devices(failed)
+
+        elif len(self.devices):
+            print("SUCCESS: All devices updated")
+
+        else:
+            print("ERROR: No device(s) available for update!")
+
+        # TODO these could be tried again if the user selects it
+        self.devices = failed
+
+    def upload(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                # connect and get device list
+                sock.connect((self.server_host, self.server_port))
+                if not self.parse_incoming(sock, 2):
+                    return
+                
+                # select which devices to update
+                to_update = self.ui_select_devices()
+                if not len(to_update):
+                    return
+                
+                # send update commands
+                commands = []
+                commands.append("update_devices=" + list_to_str(to_update))
+                commands.append("fw_m0=" + self.firmware_file_m0)
+                commands.append("fw_m4=" + self.firmware_file_m4)
+                sock.sendall(encode(list_to_str(commands, '\n')))
+                
+                # get list of updated devices
+                if not self.parse_incoming(sock, 10):
+                    return
+
+                self.ui_result()
+
+            except ConnectionRefusedError:
+                print("ERROR: Update server not running!")
+
+class Program:
+    
+    def __init__(self):
+        parser = argparse.ArgumentParser(description="Connects to a running \
+                firmware update server to update a target with a given \
+                firmware. Either specify --config or <firmware> <firmware2>")
+        parser.add_argument("--config",  help="A config file specifying one \
+                target per line. Any data up to the first whitespace (if any)\
+                is parsed as the firmware filename")
+        parser.add_argument("firmware", nargs='?', help="The firmware file \
+                to send to the device")
+        parser.add_argument("firmware2", nargs='?',  help="The secondary \
+                firmware file to send to the device")
+        parser.add_argument("-ip","--ip", type=str, help="The update server ip \
+                address to connect to (default=localhost)", default="localhost")
+        parser.add_argument("-p","--port", type=int, help="The update server \
+                port to connect to (default=3853)", default=3853)
+
+        self.parser = parser
+        self.args = None
+
+
+    def parse_args(self):
+        args = self.parser.parse_args()
+        self.args = args
+
+        updates = []
+        if not args.config is None:
+            try:
+                with open(args.config, 'r') as cfg:
+                    for line in cfg.readlines():
+                        opts = line.split()
+                        updates.append({
+                            'firmware':   opts[0],
+                            })
+            except IOError:
+                print("ERROR: failed to open config file '%s'"
+                        % args.config)
+                sys.exit()
+            
+            if not len(updates):
+                print("ERROR: no valid target(s) found in config file '%s'"
+                        % args.config)
+                self.parser.print_usage()
+                sys.exit()
+                
+        else:
+            if (args.firmware is None 
+                    or args.firmware2 is None):
+
+                print("ERROR: specify at least --config or firmware, firmware2")
+                self.parser.print_usage()
+                sys.exit()
+            else:
+                updates.append({
+                    'firmware':   args.firmware,
+                    })
+                updates.append({
+                    'firmware':   args.firmware2,
+                    })
+
+        return updates
+
+
+    def run(self):
+        
+        updates = self.parse_args()
+        
+        files = []
+        for update in updates:
+            f = update['firmware']
+            if not os.path.isfile(f):
+                print("ERROR: {} is not a file".format(f))
+                self.parser.print_usage()
+                sys.exit()
+
+            files.append(f)
+
+
+        update = Update(files[0], files[1], self.args.ip, self.args.port)
+        update.upload()
+
+if __name__ == "__main__":
+    program = Program()
+    program.run()
+
+
+
